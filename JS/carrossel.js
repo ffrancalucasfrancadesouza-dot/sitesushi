a3d8cfa0ed8c5830f2356d73620e6663aa14efe6
ffrancalucasfrancadesouza-dot <ffrancalucasfrancadesouza@gmail.com>
2026-04-22 17:48:46 -0400

    tesk21
    translate01

---
+++ JS/carrossel.js
@@ -0,0 +1,17 @@
+let index = 0
+const total = 3;
+
+function update(){
+    const carrossel_principal = document.getElementById("carrossrl-principal")
+    carrossel_principal.style.transform = `translateX(-${index * 100}%)`;
+
+}
+function next(){
+    index = (index + 1) % total;
+    update();
+
+}
+function prev(){
+    index = (index - 1) % total;
+    update();
+}
