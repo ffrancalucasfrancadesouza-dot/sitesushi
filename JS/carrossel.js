98bb26859b4c76719cc0cee2355f445f3a54812d
ffrancalucasfrancadesouza-dot <ffrancalucasfrancadesouza@gmail.com>
2026-04-24 15:07:37 -0400

    tesk21
    atualização do carrossel

--- JS/carrossel.js
+++ JS/carrossel.js
@@ -2,7 +2,7 @@ let index = 0
 const total = 3;
 
 function update(){
-    const carrossel_principal = document.getElementById("carrossrl-principal")
+    const carrossel_principal = document.getElementById("carrossel-principal")
     carrossel_principal.style.transform = `translateX(-${index * 100}%)`;
 
 }
